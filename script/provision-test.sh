05bf15cff2f44f9160e8fac6f049a7c183169680
kyleparrott <kyle@kubos.co>
2017-01-06 09:07:59 -0600

    Update the provision test script to read test program names from a file

--- script/provision-test.sh
+++ script/provision-test.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
+#README: This script only checks for programs to be installaed and accessible in the default PATH.
+#to add more programs to check for add them to the programs.txt file in the same directory.
 
 red='\E[31m'
 green='\E[32m'
-alias Reset="tput sgr0"
 
-programs=( \
-    "arm-none-eabi-gcc"    \
-    "arm-none-eabi-gdb"    \
-    "msp430-gcc"   \
-    "msp430-gdb"   \
-    "openocd"  \
-    "mspdebug" \
-    "dfu-util"  \
-    )
-
+#Only intended to run inside the vagrant box Which has bash v4.x and has the readarray command
+readarray programs < programs.txt
 
 test_installed () {
-    command -v $1 > /dev/null &&  printf "$green$1  => found\n"  ||  printf "$red$1 => not found\n" fi
-    Reset
+    command -v $1 > /dev/null &&  printf "$green$1 => found\n" ||  printf "$red$1 => not found\n"
+    tput sgr0 #reset to normal text output
 }
 
 for prog in "${programs[@]}"
