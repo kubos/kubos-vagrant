4e561438ac674f3faa54d583921fe7cde8eaa7e2
catfreed <catherine@kubos.co>
2017-01-10 16:24:22 +0000

    Adding buildroot toolchain files for KubOS Linux on iOBC

---
+++ bin/iobc_toolchain/usr/lib/gcc/arm-buildroot-linux-uclibcgnueabi/5.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/usr/bin/iobc_toolchain/usr --sysconfdir=/usr/bin/iobc_toolchain/etc --enable-static --target=arm-buildroot-linux-uclibcgnueabi --with-sysroot=/usr/bin/iobc_toolchain/usr/arm-buildroot-linux-uclibcgnueabi/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/usr/bin/iobc_toolchain/usr --with-mpc=/usr/bin/iobc_toolchain/usr --with-mpfr=/usr/bin/iobc_toolchain/usr --with-pkgversion='Buildroot 2016.11' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --disable-libsanitizer --enable-tls --enable-plugins --enable-lto --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --disable-decimal-float --with-abi=aapcs-linux --with-cpu=arm926ej-s --with-float=soft --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/usr/bin/iobc_toolchain/usr/arm-buildroot-linux-uclibcgnueabi/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "arm926ej-s" }, { "float", "soft" }, { "mode", "arm" }, { "tls", "gnu" } };
