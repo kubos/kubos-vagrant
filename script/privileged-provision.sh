cb65f54a55af4a6d2f48cf91c6de3628795ea3e2
kyleparrott <kyle@kubos.co>
2017-01-24 15:38:49 -0600

    removing the temporary kubos-cli installation steps. Re-arragning and simplifying the provisioning process.

--- script/privileged-provision.sh
+++ script/privileged-provision.sh
@@ -1,11 +1,9 @@
 #These start an interactive prompt - I can't figure out how to fix it yet...
 sudo apt-mark hold grub-common grub-pc grub-pc-bin grub2-common
 
-apt-get update
 yes | apt-get upgrade -y #Pipe in yes beacause a package still requires more input beyond the -y option
-apt-get install -y software-properties-common
 apt-get -y dist-upgrade
-apt-get update
+apt-get install -y software-properties-common
 
 add-apt-repository -y ppa:team-gcc-arm-embedded/ppa
 add-apt-repository -y ppa:george-edison55/cmake-3.x
@@ -61,12 +59,14 @@ echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 #KubOS Linux setup
 echo "Installing KubOS Linux Toolchain"
+
 apt-get install -y minicom
 apt-get install -y libc6-i386 lib32stdc++6 lib32z1
+
 tar -xf /home/vagrant/iobc_toolchain.tar.gz -C /usr/bin
+rm /home/vagrant/iobc_toolchain.tar.gz
 mv /home/vagrant/minirc.kubos /etc/minicom/minirc.kubos
 mv /home/vagrant/ftdi-usb.rules /etc/udev/rules.d/ftdi-usb.rules
-rm /home/vagrant/iobc_toolchain.tar.gz
 echo "export PATH=/usr/bin/iobc_toolchain/usr/bin:$PATH" >> /etc/profile
 
 mkdir -p /usr/local/lib/yotta_modules
