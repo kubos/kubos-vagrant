37e62c838cf8e56e14a46b1fbdc15009448841f9
kyleparrott <kyle@kubos.co>
2017-04-28 16:51:06 -0500

    Adding environment settings to make tab completion work for the Kubos CLI

--- kubos-dev/script/non-privileged-provision.sh
+++ kubos-dev/script/non-privileged-provision.sh
@@ -1,3 +1,5 @@
 kubos update --latest || true #Can't run as root or else yotta symlinks are created to /root/.kubos/...
 
+echo "source /etc/bash_completion.d/python-argcomplete.sh" >> /home/vagerant/.bashrc
+
 echo "Finishing provisioning..."
