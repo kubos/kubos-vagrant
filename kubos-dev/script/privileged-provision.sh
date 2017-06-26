6615b0abbad304f5082d0220f278416cbd4b9175
kyleparrott <kyle@kubos.co>
2017-06-26 09:25:46 -0500

    Adding pyserial

--- kubos-dev/script/privileged-provision.sh
+++ kubos-dev/script/privileged-provision.sh
@@ -1,4 +1,4 @@
-pip install pysocks #Temporary - This will be added to the kubos-cli requirements.txt
+pip install pysocks pyserial #Temporary - This will be added to the kubos-cli requirements.txt
 pip install git+https://github.com/kubostech/kubos-cli
 
 mkdir -p /usr/local/lib/yotta_modules
