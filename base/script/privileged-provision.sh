cf3f276d420023678023519e874837b29b17241e
Ryan Plauche <ryan@kubos.co>
2017-10-10 10:08:51 -0400

    Correcting dbus dependency.

--- base/script/privileged-provision.sh
+++ base/script/privileged-provision.sh
@@ -17,7 +17,7 @@ apt-get install -y gcc-arm-embedded
 apt-get install -y git
 apt-get install -y cmake
 apt-get install -y gcc-msp430 gdb-msp430 msp430-libc
-apt-get install -y libdbus1-dev
+apt-get install -y libdbus-1-dev
 
 # flash tools
 apt-get install -y mspdebug
