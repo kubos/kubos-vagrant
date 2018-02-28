edf1925f313d00ac4f288aefa4139b2db516ae56
Ryan Plauche <ryan@kubos.co>
2018-02-28 09:50:20 -0600

    Correctly copying in cargo_config file. Installs correct toolchain for iobc.

--- kubos-dev/script/non-privileged-provision.sh
+++ kubos-dev/script/non-privileged-provision.sh
@@ -1,17 +1,19 @@
 kubos update --latest || true #Can't run as root or else yotta symlinks are created to /root/.kubos/...
 
-#Install rust stuff
-#We do this as vagrant because it
-#installs to $HOME/
+# Install rust stuff
+# We do this as vagrant because it
+# installs to $HOME/
 # Rust toolchain + Cargo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo 'export PATH=$PATH:"~/.cargo/bin"' >> /home/vagrant/.bashrc
 # bbb/mbm2 target
 /home/vagrant/.cargo/bin/rustup target install arm-unknown-linux-gnueabihf
 # iobc target
-/home/vagrant/.cargo/bin/rustup target install arm-unknown-linux-gnueabi
+/home/vagrant/.cargo/bin/rustup target install armv5te-unknown-linux-gnueabi
 # install cargo-kubos
-cd /home/vagrant/.kubos/kubos/cargo-kubos/ && /home/vagrant/.cargo/bin/cargo install
+/home/vagrant/.cargo/bin/cargo install --git https://github.com/kubos/cargo-kubos
+# setup cargo config
+mv /home/vagrant/cargo_config /home/vagrant/.cargo/config
 
 kubos update --tab-completion
 
