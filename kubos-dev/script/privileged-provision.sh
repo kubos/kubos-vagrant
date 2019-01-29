1c51317ffbb6eb87a3d018679e2625b1b3454659
Catherine Garabedian <catherine@kubos.co>
2019-01-29 14:14:04 -0600

    Adding Python HTTP dependencies

--- kubos-dev/script/privileged-provision.sh
+++ kubos-dev/script/privileged-provision.sh
@@ -5,6 +5,8 @@ pip install idna==2.6
 pip install toml
 pip install graphene
 
+python3 -m pip install flask
+python3 -m pip install flask_graphql
 python3 -m pip install graphene
 python3 -m pip install mock
 python3 -m pip install toml
