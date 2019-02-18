dee251e23a72530f1f86d0f61e4f397006c41fe5
Ryan Plauche <ryan@kubos.co>
2019-02-18 12:55:30 -0600

    Added pkg-config to base. Updated provision-test on kubos-dev to test rust and python source.

--- kubos-dev/script/provision-test.sh
+++ kubos-dev/script/provision-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #README: This script only checks for programs to be installed and accessible in the default PATH.
 #To add more programs to check for add them to the programs array.
+#Also runs standard CI tests to check if dev dependencies are installed and available
+
+set -e
 
 red='\E[31m'
 green='\E[32m'
@@ -10,6 +13,9 @@ programs=(
     lsusb
     python
     python3
+    kubos-file-client
+    kubos-shell-client
+    uart-comms-client
 )
 
 test_installed () {
@@ -26,3 +32,15 @@ for prog in "${programs[@]}"
 do
     test_installed $prog
 done
+
+git clone https://github.com/kubos/kubos --depth 1
+cd kubos
+
+/home/vagrant/.cargo/bin/cargo test
+python3 tools/ci_c.py
+python3 hal/python-hal/i2c/test_i2c.py
+cd apis/pumpkin-mcu-api; python3 test_mcu_api.py; cd ../..
+cd apis/app-api/python; python3 test_app_api.py; cd ../../..
+
+cd ..
+rm -rf kubos
