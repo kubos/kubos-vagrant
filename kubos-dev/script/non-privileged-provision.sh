74f3e549a24b4aed85c229f9458de1bd78d28a84
Catherine Garabedian <catherine@kubos.co>
2019-08-22 10:32:20 -0500

    Cleaning up target dir after install

--- kubos-dev/script/non-privileged-provision.sh
+++ kubos-dev/script/non-privileged-provision.sh
@@ -8,6 +8,8 @@ echo 'export PATH=$PATH:"~/.cargo/bin"' >> /home/vagrant/.bashrc
 # install rust tools
 /home/vagrant/.cargo/bin/rustup component add clippy
 /home/vagrant/.cargo/bin/rustup component add rustfmt
+# Remove the toolchain docs because we don't need them and they take up 600MB of space
+rm -rf /home/vagrant/.rustup/*/share/doc
 # bbb/mbm2 target
 /home/vagrant/.cargo/bin/rustup target install arm-unknown-linux-gnueabihf
 # iobc target
@@ -22,6 +24,8 @@ mv /home/vagrant/cargo_config /home/vagrant/.cargo/config
 /home/vagrant/.cargo/bin/cargo install --bin kubos-shell-client --path /home/vagrant/.kubos/kubos/clients/kubos-shell-client/
 # Install example UART comms client
 /home/vagrant/.cargo/bin/cargo install --bin uart-comms-client --path /home/vagrant/.kubos/kubos/clients/uart-comms-client/
+# Cleanup temporary build files
+rm -rf /home/vagrant/.kubos/kubos/target
 
 # Install app-api python module
 cd /home/vagrant/.kubos/kubos/apis/app-api/python && python3 -m pip install .
